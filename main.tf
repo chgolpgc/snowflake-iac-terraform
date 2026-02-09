065ede4e1c8b0cdf79474b70d628d27e92b119f8
Christopher González Delgado <christopher.gonzalez@plainconcepts.com>
2026-02-09 11:53:35 +0100

    test CI with state

--- main.tf
+++ main.tf
@@ -1,9 +1,18 @@
 terraform {
+  required_version = ">= 1.3.0"
+
   required_providers {
     snowflake = {
       source  = "snowflakedb/snowflake"
       version = ">= 1.0.0"
     }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "fabric_related_rg"
+    storage_account_name = "sathesnowflakejournal"
+    container_name       = "tfstate"
+    key                  = "terraform-snowflake.tfstate"
   }
 }
 
